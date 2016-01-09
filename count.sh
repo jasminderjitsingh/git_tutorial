641dd75337ea6e534786c01f3726f8cf37bde912
Jasminderjit Singh <jasminderjitsingh89@gmail.com>
2016-01-09 13:31:57 -0500

    code changed

--- count.sh
+++ count.sh
@@ -1,6 +1,6 @@
 # Display the name of each data file and count
 # the number of distinct occurrences of each species
-# in that file
+# in that file.
 for filename in data/*.txt
 do
     echo $filename
